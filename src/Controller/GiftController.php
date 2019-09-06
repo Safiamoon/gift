1a833302a62fdb37bef10f4646e3d5d3906aeeb7
Safia <msellek.safia@gmail.com>
2019-09-06 17:00:28 +0200

    Added an authentification form

---
+++ src/Controller/GiftController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GiftRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GiftController extends AbstractController
+{
+    /**
+     * @Route("/gifts", name="gifts")
+     */
+    public function index(GiftRepository $giftRepository)
+    {
+        $gifts = $giftRepository->findAll();
+        return $this->render('gift/index.html.twig', [
+           'gifts'=>$gifts
+        ]);
+    }
+
+
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,32 @@
 namespace App\DataFixtures;
 
 use App\Entity\Gift;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    { 
+
+        $user = new User();
+        $user->setEmail('test@test.com')
+             ->setRoles(['ROLE_ADMIN'])
+             ->setPassword($this->encoder->encodePassword($user, "helloworld"));
+
+        $manager->persist($user);
+        $manager->flush();
+
+
         $faker = Factory::create('fr_FR');
         
         for ($i = 1; $i < 20; $i++) {
